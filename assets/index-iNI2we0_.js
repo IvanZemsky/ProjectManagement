cc3259469b9533067fc31fc4d103b8d0dd1016e0
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-11-23 01:23:08 +0300

    Updates

--- assets/index-iNI2we0_.js
+++
@@ -1,1 +0,0 @@
-import{u as o,j as t,N as e,s as r,R as a}from"./index-CALslcgW.js";const c=()=>{const{projectId:s}=o();return s?t.jsx(e,{to:r("",a.Projects,s),replace:!0}):t.jsx(e,{to:r("",a.Projects),replace:!0})};export{c as default};
